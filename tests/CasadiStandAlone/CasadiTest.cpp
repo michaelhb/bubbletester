c66f61cbcb6564abcd9f7920d149db3e1e804f8c
Michael Bardsley <michael.bardsley@monash.edu>
2020-02-14 18:02:20 +1100

    C++ port works, but it's slow??

--- tests/CasadiStandAlone/CasadiTest.cpp
+++ tests/CasadiStandAlone/CasadiTest.cpp
@@ -17,7 +17,8 @@ std::string varname(std::string prefix, std::vector<int> indices) {
     return ss.str();
 }
 
-void append_d(std::vector<double> to, std::vector<double> from) {
+void append_d(std::vector<double> &to, std::vector<double> from) {
+    // Yes I know this is a weird function signature :)
     to.insert(to.end(), from.begin(), from.end());
 }
 
@@ -59,7 +60,7 @@ void solve(Ca_Potential ca_potential, DM false_vac, DM true_vac) {
     SX potential = std::get<0>(ca_potential);
     SX phi = std::get<1>(ca_potential);
     int n_phi = phi.size1();
-    
+
     // Wrap potential as callable function & get value @ true vacuum
     Function fV = Function("fV", {phi}, {potential}, {"phi"}, {"V(phi)"});
     DM v_true = fV(false_vac);
@@ -75,10 +76,12 @@ void solve(Ca_Potential ca_potential, DM false_vac, DM true_vac) {
     int d = 3;
 
     // Linear ansatz
-    auto ansatz = [T, false_vac, true_vac](double t) {return (t/T)*false_vac + (1 - t/T)*true_vac;};
+    auto ansatz = [T, false_vac, true_vac](double t) {
+        return ((t/T)*false_vac + (1 - t/T)*true_vac).get_elements();
+    };
 
     // Derivative of ansatz is a constant
-    DM phidot_ansatz = (1/T)*(false_vac - true_vac);
+    std::vector<double> phidot_ansatz = ((1/T)*(false_vac - true_vac)).get_elements();
 
     // Set up the collocation points
     std::vector<double> tau_root = collocation_points(d, "legendre");
@@ -124,7 +127,9 @@ void solve(Ca_Potential ca_potential, DM false_vac, DM true_vac) {
     SX u = SX::sym("u", n_phi);
 
     // Define the integrand in the objective function
-    Function L = Function("L", {phi, u}, {sqrt(2*SX::minus(fV(phi)[0], v_true))*norm_2(u)});
+    Function L = Function("L", 
+        {phi, u}, {sqrt(2*SX::minus(fV(phi)[0], v_true))*norm_2(u)},
+        {"phi", "u"}, {"L(phi, u)"});
 
     // Dynamics function (trivial here, just u = phidot)
     Function f = Function("f", {phi, u}, {u}, {"phi", "u"}, {"phidot"});
@@ -137,13 +142,13 @@ void solve(Ca_Potential ca_potential, DM false_vac, DM true_vac) {
     std::vector<MX> g = {}; // All constraint functions
     std::vector<double> lbg = {}; // Lower bounds for constraints
     std::vector<double> ubg = {}; // Upper bounds for constraints
-    SX J = 0; // Objective function
+    MX J = 0; // Objective function
 
     // Limits for unbounded variables
     std::vector<double> ubinf(n_phi, inf);
     std::vector<double> lbinf(n_phi, -inf);
 
-    // Initialise control variables
+    /**** Initialise control variables ****/
     std::vector<MX> controls = {}; 
     for (int k = 0; k < N; ++k) {
         MX Uk = MX::sym(varname("U", {k}), n_phi);
@@ -151,9 +156,110 @@ void solve(Ca_Potential ca_potential, DM false_vac, DM true_vac) {
         w.push_back(Uk);
         append_d(lbw, lbinf);
         append_d(ubw, ubinf);
-        // append_d(w0, phidot_ansatz.get);
+        append_d(w0, phidot_ansatz);
     }
 
+    /**** Initialise state variables ****/
+
+    // Start with initial state fixed to true vacuum
+    MX phi_0_0 = MX::sym("phi_0_0", n_phi);
+    w.push_back(phi_0_0);
+    append_d(lbw, true_vac.get_elements());
+    append_d(ubw, true_vac.get_elements());
+    append_d(w0, true_vac.get_elements());
+
+    // Free endpoint states
+    std::vector<MX> endpoints;
+    endpoints.push_back(phi_0_0);
+    for (int k = 1; k < N; ++k) {
+        MX phi_k_0 = MX::sym(varname("phi", {k, 0}), n_phi);
+        endpoints.push_back(phi_k_0);
+        w.push_back(phi_k_0);
+        append_d(lbw, lbinf);
+        append_d(ubw, ubinf);
+        append_d(w0, ansatz(t_kj(k, 0)));
+    }
+
+    // Final state, fixed to the false vacuum
+    MX phi_N_0 = MX::sym("phi_N_0", n_phi);
+    endpoints.push_back(phi_N_0);
+    w.push_back(phi_N_0);
+    append_d(lbw, false_vac.get_elements());
+    append_d(ubw, false_vac.get_elements());
+    append_d(w0, false_vac.get_elements());
+
+    // Intermediate free states at collocation points
+    std::vector<std::vector<MX>> collocation_states;
+    for (int k = 0; k < N; ++k) {
+        std::vector<MX> k_states;
+        for (int j = 1; j <= d; ++j) {
+            MX phi_k_j = MX::sym(varname("phi", {k, j}), n_phi);
+            k_states.push_back(phi_k_j);
+            w.push_back(phi_k_j);
+            append_d(lbw, lbinf);
+            append_d(ubw, ubinf);
+            append_d(w0, ansatz(t_kj(k, j)));
+        }
+        collocation_states.push_back(k_states);
+    }
+
+    /**** Implement the constraints ****/
+
+    // Zero vector for constraint bounds
+    std::vector<double> zeroes(n_phi, 0);
+
+    // Continuity equations
+    for (int k = 0; k < N; ++k) {
+        // Approximation of phi_(k+1)_0 using the k-domain collocation points
+        MX phi_end = D[0]*endpoints[k];
+        for (int j = 1; j <= d; ++j) {
+            phi_end += D[j]*collocation_states[k][j - 1];
+        }
+
+        // We require that this is equal to the the actual value of phi_(k+1)_0
+        g.push_back(phi_end - endpoints[k + 1]);
+        append_d(lbg, zeroes);
+        append_d(ubg, zeroes);
+    }
+
+    // Collocation equations
+    for (int k = 0; k < N; ++k) {
+        for (int j = 1; j <= d; ++j) {
+            // Approximation of the state derivative at each collocation point
+            MX phidot_approx = C[0][j]*endpoints[k];
+            for (int r = 0; r < d; ++r) {
+                phidot_approx += C[r + 1][j]*collocation_states[k][r];
+            }
+
+            // We relate this to the derivative (control) U_k
+            g.push_back(h*controls[k] - phidot_approx);
+            append_d(lbg, zeroes);
+            append_d(ubg, zeroes);
+        }
+    }
+
+    /**** Construct the objective function ****/
+    for (int k = 0; k < N; ++k) {
+        for (int j = 1; j <= d; ++j) {
+            std::vector<MX> arg = {collocation_states[k][j - 1], controls[k]};
+            MX dL = L(arg)[0];
+            J = J + B[j]*dL*h;
+        }
+    }
+
+    /**** Initialise and solve the NLP ****/
+    
+    // Collect states and constraints into single vectors
+    MX W = MX::vertcat(w);
+    MX G = MX::vertcat(g);
+
+    // Create the solver
+    MXDict nlp = {{"f", J}, {"x", W}, {"g", G}};
+    Function solver = nlpsol("nlpsol", "ipopt", nlp);
+
+    DMDict arg = {{"x0", w0}, {"lbx", lbw}, {"ubx", ubw}, {"lbg", lbg}, {"ubg", ubg}};
+
+    DMDict res = solver(arg);
 }
 
 };
